b86a935e227235e0ad81e7879d89c8cb6d3a6502
VISHWANATH KRISHNAPPA HUGAR <119155604+Vishwa659@users.noreply.github.com>
2025-07-18 22:22:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,13 @@ const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('./dataStorage.db');
 
-db.run(`INSERT INTO dataStorage (phoneNumber, email, linkedId, linkPrecedence, createdAt, updatedAt, deletedAt)
-VALUES (?, ?, ?, ?, ?, ?, ?)`,
-[
-  '1234567890',
-  'test@gmail.com',
-  null,
-  'secondary',
-  '2023-04-01 00:00:00.374+00:00',
-  '2023-04-01 00:00:00.374+00:00',
-  null
-]);
-
-module.exports = db;
-
+db.run(`CREATE TABLE IF NOT EXISTS dataStorage (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  email TEXT,
+  phoneNumber TEXT,
+  linkedId INTEGER,
+  linkPrecedence TEXT,
+  createdAt TEXT,
+  updatedAt TEXT,
+  deletedAt TEXT
+)`);
